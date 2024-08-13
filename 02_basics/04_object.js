dbc21984b08669af5bebaf63dcd08a823e24275d
abhaychaudhary <abhaychaudharyrc@gmail.com>
2024-08-14 00:10:29 +0530

    json

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -52,4 +52,32 @@ console.log(tinderUser);
 console.log(Object.keys(tinderUser));
 console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
-console.log(tinderUser.hasOwnProperty('isLogged'));
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+const course ={
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor : "abhay"
+
+}
+// course.courseInstructor
+
+const{courseInstructor : instructor} = course 
+//console.log(courseInstructor);
+console.log(instructor);
+
+
+
+//JSON FORMAT 
+// {
+//     "name" : "abhay",
+//     "course" : "hindi",
+//     "price" : "free"
+// }
+[
+    {},
+    {},
+    {}
+]
+
