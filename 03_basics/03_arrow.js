cdb54877e9b530d2eea35979e55369236078bbf2
abhaychaudhary <abhaychaudharyrc@gmail.com>
2024-09-03 01:46:15 +0530

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,56 @@
+const user = {
+    username : "abhay",
+    price : 999,
+
+    welcomeMessage : function(){
+        console.log(`${this.username}, welcome to website`);
+        
+        console.log(this);
+        
+    }
+    
+    
+}
+
+// user.welcomeMessage()
+
+// user.username = "sam"
+// user.welcomeMessage()
+
+//console.log(this);
+
+
+// function chai(){
+//     let username = "abhay"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "abhay"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+const chai = () =>{
+    let username = "abhay"
+    console.log(this);
+    
+}
+
+//chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+//const addTwo = (num1, num2) => num1 + num2
+//const addTwo = (num1, num2) => (num1 + num2)
+const addTwo = (num1, num2) => ({username : "abhay"})
+
+console.log(addTwo(3,4));
+
+// const myArray = [2,3,5,7,9]
+
+// myArray.forEach()
