90400070d4895fd7aab315599232f14500a31156
abhaychaudhary <abhaychaudharyrc@gmail.com>
2024-09-03 01:16:58 +0530

    function

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,64 @@
+// let a = 10
+// const b = 20
+// var c = 30
+// let a = 300
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     console.log("ineer :" , a);
+    
+// }
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+// console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+function one (){
+    const username = "abhay"
+    function two(){
+        const website = " youtube "
+        console.log(username);
+        
+    }
+    //console.log(website);
+    //two()
+    
+}
+
+one()
+
+
+if (true){
+    const username = "abhay"
+    if(username === "abhay"){
+        const website = "youtube"
+      //  console.log(username  +  website);
+        
+    }
+   // console.log(website);
+    
+}
+//console.log(username);
+
+
+
+// +++++++++++++++++++++++  intresting +++++++++++++++++++++++=
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+
+addtwo(5)
+const addtwo = function(num){
+    return num + 2
+
+}
+
+//addtwo(5)
