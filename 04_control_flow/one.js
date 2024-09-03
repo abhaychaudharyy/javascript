e8e32eceef00cc3b5572707f5f8b4ade90687a5a
abhaychaudhary <abhaychaudharyrc@gmail.com>
2024-09-03 23:38:41 +0530

    nullish

---
+++ 04_control_flow/one.js
@@ -0,0 +1,37 @@
+//if
+//const isUserLoggedIn = true
+// temperature = 41
+// if (temperature ===40) {
+//     console.log("less than 50");
+    
+// } else {
+//     console.log("temprature is greater than 50");
+    
+// }
+// console.log("executed");
+
+// const score = 200
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`User power : ${power}`);
+    
+// }
+// console.log(`User power : ${power}`);
+//<, > ,<=, >=, ==,!=, ===
+
+
+// 
+
+const isUserLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+if (isUserLoggedIn && debitCard && 2 == 3){
+    console.log("Allow to buy course");
+    
+}
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user loggedIn");
+    
+
+}
